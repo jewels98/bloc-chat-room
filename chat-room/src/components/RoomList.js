c503d02122b40e790bd39bf7dcad611337fdbc9d
jewels98 <mrwright0825@gmail.com>
2018-08-17 18:29:50 -0400

    Added texts form

--- chat-room/src/components/RoomList.js
+++ chat-room/src/components/RoomList.js
@@ -7,36 +7,64 @@ class RoomList extends Component {
 //  store the list of rooms, so add an array of rooms on component state object
 
         this.state = {
-            rooms: []
+            rooms: [],
+            newRoomName: '',
         };
+
 // store firebase reference to rooms path onto the this keyword
 
 this.roomsRef = this.props.firebase.database().ref('rooms');
+this.updateRooms = this.updateRooms.bind(this);
+this.handleChange = this.handleChange.bind(this);
+this.handleSubmit = this.handleSubmit.bind(this);
 
     }
+
+    up
 
     //  Add componentDidmount method / register child_added event
     //  to this.roomRef
 
     componentDidMount() {
+        this.updateRooms();
+    }
+    updateRooms(){
         this.roomsRef.on('child_added', snapshot => {
             const room = snapshot.val();
             room.key = snapshot.key;
-            this.setState({ rooms: this.state.rooms.concat (room)})
+            this.setState({ rooms: this.state.rooms.concat (room)});
         });
     }
+
+        handleChange(ev){
+            this.setState({ newRoomName: ev.target.value });
+        }
+
+        handleSubmit(ev) {
+            ev.preventdefault();
+            this.roomsRef.push().set({ name: this.state.newRooName });
+        }
+
+
     render(){
         // array map over state.rooms and return a div with room.name for each array item
-
-        let roomListRender = this.state.rooms.map((room) => {
-            return<div key={room.name}>
-                {room.name}
-            </div>
-        })
-
         return (
-            <div>{roomListRender}</div>
-        )
+            <div className="room-list">
+                <h2>Welcome!</h2>
+                <div id="rooms">
+                    {this.state.rooms.map((room, index) => (
+                     <h2 key={index}>{room.name}</h2>   
+                    ))}
+                </div>
+                <form  classname="room-form" onSubmit={e => this.handleSubmit(e)}>
+                    <input type="text"
+                        value={this.state.newRoomName}
+                        onChange={e => this.handleChange(e)}
+                    />
+                    <input type="submit" value="Create a new room" />
+                </form>
+            </div> 
+        );
     }
 }
 
